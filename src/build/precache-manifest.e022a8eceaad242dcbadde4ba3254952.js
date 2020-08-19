10c4690bf0d657f6489afcdc22f1e39c982956ac
isyel <isykaal4saviour@gmail.com>
2020-08-19 15:19:49 +0100

    first commit of full app

---
+++ src/build/precache-manifest.e022a8eceaad242dcbadde4ba3254952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88883afa8e16cbb77929e662af94300c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0985b8013ba6d919667",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "f75decdb673575fc346a",
+    "url": "/static/js/2.31682741.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.31682741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0985b8013ba6d919667",
+    "url": "/static/js/main.63cf4b4a.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
